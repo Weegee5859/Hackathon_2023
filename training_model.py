38c41aac0f7a402dd6d3bce1d9fb2e56c7c6aa4c
Z3NT61 <zenzedd@gmail.com>
2023-10-07 13:25:33 -0400

    commit

--- training_model.py
+++ training_model.py
@@ -2,14 +2,14 @@ import os
 import openai
 
 openai.api_key = "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"
-
+# my API KEY I spent 10 bucks on credits sigh
 openai.File.create(file=open("training_data.json", "r"), purpose='fine-tune')
 
 model_engine = "gpt-3.5-turbo"
-batch_size = 5
-n_epochs = 1
-learning_rate = 0.01
-max_tokens = 2048
+batch_size = 5  # How many times GPT runs through our data set
+n_epochs = 1  # How many times GPT changes based on each data set
+learning_rate = 0.01  # Honestly no idea but just how fast it learns
+max_tokens = 2048  # How many characters we can pass into GPT
 
 training_file = "training_data.json"
 
