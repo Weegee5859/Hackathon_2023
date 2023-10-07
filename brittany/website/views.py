2fe8419c4844ef57b2cc65819776e747e8bb0467
Joshua Marshall <weegee5859@gmail.com>
2023-10-07 13:40:23 -0400

    homepage initalized
    initalize homepage and homepage css

--- brittany/website/views.py
+++ brittany/website/views.py
@@ -1,10 +1,11 @@
 #store standard routes for wesbites
 #where users can go to (navigation)
 
-from flask import Blueprint #URLs defined
+from flask import Blueprint, render_template #URLs defined
+
 
 views = Blueprint('views', __name__) #define blueprint
 
 @views.route('/') #main page 
 def home():
-    return "<h1>Test</h1>"
+    return render_template('home.html')
