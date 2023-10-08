24c0fb45112bb5a06c70d806577951176c76e6d3
Z3NT61 <zenzedd@gmail.com>
2023-10-08 10:53:26 -0400

    final commit

--- brittany/brmain.py
+++ brittany/brmain.py
@@ -1,4 +1,4 @@
-from website import create_app #used from __init__.py
+from website import create_app  # used from __init__.py
 from flask import Flask, jsonify, render_template, request
 import openai
 import os
@@ -6,6 +6,7 @@ import os
 openai.api_key = "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"
 
 app = create_app()
+
 
 def gpt(input):
     messages = [{"role": "system", "content": ""}]
@@ -17,14 +18,14 @@ def gpt(input):
                 {"role": "user", "content": message},
             )
             chat = openai.ChatCompletion.create(
-                model="ft:gpt-3.5-turbo-0613:personal::87N7eqKL",
+                model="ft:gpt-3.5-turbo-0613:personal::87PDCxlf",
                 messages=messages
             )
         reply = chat.choices[0].message.content
         print(f"ChatGPT: {reply}")
         messages.append({"role": "assistant", "content": reply})
         return reply
-    
+
 
 @app.route("/gpt_call", methods=['POST', 'GET'])
 def gpt_call():
@@ -33,12 +34,13 @@ def gpt_call():
         print(my_data[0]["input"])
         gpt_response = gpt(my_data[0]["input"])
         print(gpt_response)
- 
+
     results = {'processed': gpt_response}
     return jsonify(results)
 
-if __name__ == '__main__': #only if run file
-    #run flask application to start up web server
-    app.run(port=12000, debug=True) 
-    #True = changes in code with rerun webserver
-    #False = production
+
+if __name__ == '__main__':  # only if run file
+    # run flask application to start up web server
+    app.run(port=12000, debug=True)
+    # True = changes in code with rerun webserver
+    # False = production
