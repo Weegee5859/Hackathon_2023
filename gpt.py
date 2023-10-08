00cf14277e74880eaa359377f4c52a1ca53e17e3
Z3NT61 <zenzedd@gmail.com>
2023-10-08 08:25:15 -0400

    gpt is now a function call

--- gpt.py
+++ gpt.py
@@ -4,30 +4,21 @@ import os
 
 openai.api_key = "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"
 
-messages = [{"role": "system", "content":
-             "You are a intelligent assistant."}]
 
-while True:
-    message = input("User : ")
-    if message:
+def gpt(input):
+    messages = [{"role": "system", "content": ""}]
 
-        if ((random.SystemRandom().randint(0, 100) % 2) == 1):
-            print("2")
+    while True:
+        message = input
+        if message:
             messages.append(
-                {"role": "user", "content": "\" " + message +
-                 "\" respond but in a short slightly blunt sentence"},
+                {"role": "user", "content": message},
             )
-        else:
-            print("1")
-            messages.append(
-                {"role": "user", "content": "\" " + message +
-                 "\" respond but in a short short humorous sentence"},
+            chat = openai.ChatCompletion.create(
+                model="ft:gpt-3.5-turbo-0613:personal::87GPAaIE",
+                messages=messages
             )
-        chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages
-
-        )
-    reply = chat.choices[0].message.content
-    print(f"ChatGPT: {reply}")
-    messages.append({"role": "assistant", "content": reply})
+        reply = chat.choices[0].message.content
+        print(f"ChatGPT: {reply}")
+        messages.append({"role": "assistant", "content": reply})
+        return reply
